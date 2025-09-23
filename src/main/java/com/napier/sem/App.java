151263f85fb329df51cf26e4f00b9dce092edba0
40794419ThantLwinMaung <40794419@live.napier.ac.uk>
2025-09-23 15:13:38 +0630

    Add employee management functionality: implement Employee class and update App to connect to MySQL database and retrieve employee details

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,18 +1,141 @@
 package com.napier.sem;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App
 {
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(3000);
+                // Connect to database
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/employees?useSSL=false",
+                        "root",
+                        "example"
+                );
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + i);
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                con.close();
+                System.out.println("Disconnected from database");
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+    /**
+     * Get employee information from database.
+     * @param ID Employee ID to lookup
+     * @return Employee object or null if not found
+     */
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE emp_no = " + ID;
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Check if employee exists
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+            {
+                System.out.println("Employee not found");
+                return null;
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
     public static void main(String[] args)
     {
-        // Create new Connect object
-        Connect db = new Connect();
+        App a = new App();
 
         // Connect to database
-        db.connect();
+        a.connect();
 
-        // Here you could run queries using db.getConnection()
+        // Example: Get employee with ID 10001
+        Employee emp = a.getEmployee(10001);
+        if(emp != null)
+        {
+            System.out.println(emp.emp_no + ": " + emp.first_name + " " + emp.last_name);
+        }
 
         // Disconnect from database
-        db.disconnect();
+        a.disconnect();
     }
 }
